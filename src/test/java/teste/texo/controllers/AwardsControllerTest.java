30c22ca44c854a8b9c9a499d770cc05b11bf0219
Estefanio <fanio1909@hotmail.com>
2019-12-15 12:14:08 -0300

    Melhora teste de integração

--- src/test/java/teste/texo/controllers/AwardsControllerTest.java
+++ src/test/java/teste/texo/controllers/AwardsControllerTest.java
@@ -1,6 +1,7 @@
 package teste.texo.controllers;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import kong.unirest.HttpResponse;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -18,6 +19,16 @@ public class AwardsControllerTest extends ControllerTest {
         HttpResponse<JsonElement> response = get("awards", null);
 
         assertEquals(200, response.getStatus());
+
+        JsonObject body = response.getBody().getAsJsonObject();
+
+        assertNotNull(body.get("min"));
+        assertTrue(body.get("min").getAsJsonArray().size() > 0);
+        assertEquals("Bo Derek", body.get("min").getAsJsonArray().get(0).getAsJsonObject().get("producer").getAsString());
+
+        assertNotNull(body.get("max"));
+        assertTrue(body.get("max").getAsJsonArray().size() > 0);
+        assertEquals("Matthew Vaughn", body.get("max").getAsJsonArray().get(0).getAsJsonObject().get("producer").getAsString());
     }
 
 }
